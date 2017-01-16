c755d61d19e369220a26471cc23a8a50b00d4451
Ben Kambach <bkambach@outlook.com>
2017-01-16 17:54:46 -0600

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello New 5 World");
+  res.send("Hello New 6 World");
 });
 
 app.listen(4000);
